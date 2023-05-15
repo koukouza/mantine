17bbb10fe93625c7536c61a06d205d3087eb9c2d
boukaoutar <boulguidkaoutar@gmail.com>
2023-05-15 15:13:16 +0200

    mode sombre

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MantineProvider, Button } from '@mantine/core';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+    theme={{
+      colorScheme: 'dark'
+    }}>
+      <App />
+    </MantineProvider>
+    
   </React.StrictMode>
 );
 
